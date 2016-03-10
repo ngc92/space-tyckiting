a7cbddb97059370aed2fbe7e8fd9f93ad7c5f4f1
ngc92 <erik-schultheis@freenet.de>
2016-03-10 18:03:55 +0200

    updated dummy bot

--- clients/julia/tyckiting_client/ai/dummy.jl
+++ clients/julia/tyckiting_client/ai/dummy.jl
@@ -2,22 +2,25 @@ module Dummy_AI
 
 using ClientAI
 
-# include several AI utility functions and classes.
-include("../AIUtil/AIUtil.jl")
-using .AIUtil
-
+# the DummyAI class
 type DummyAI <: AbstractAI
   config::Config
   bots::Vector{AbstractBot}
 end
 
+# This function is called when the game starts and gets information about own and enemy bots.
+function on_start(ai::DummyAI, own_bots::Vector, enemy_bots::Vector)
+end
+
+# This function is called at the beginning of every round, and gets information about own bots and
+# the events that happend in the last round. It is called before events are processed by the 
+# on_event handlers.
 function init_round(ai::DummyAI, bots::Vector{AbstractBot}, events::Vector{AbstractEvent}, round_id::Integer)
   ai.bots = bots
 end
 
-function on_start(ai::DummyAI, own_bots::Vector, enemy_bots::Vector)
-end
-
+# this function is called after event handling and has to return an array of actions that the bot wants to
+# perform.
 function decide(ai::DummyAI)
   bots = filter_valid(ai.bots)
   actions = AbstractAction[]
